d13fac0e4d6112b1b4bb7d964f1fe302020df4fe
tfjackc <jack.colpitt@crookcountyor.gov>
2023-08-04 13:07:52 -0700

    url buttons, node sizing

--- pages/00_home.py
+++ pages/00_home.py
@@ -5,133 +5,141 @@ import solara
 import solara.lab
 import random
 import numpy as np
-# Assuming you have already imported the necessary libraries and read the data
-dfwm = pd.read_csv("data/portalWebMaps_Test.csv")
-dfsubset = dfwm[['map_title', 'service_title', 'layer_url', 'share_settings', 'number_of_views']]
+# https://geo.co.crook.or.us/portal/home/item.html?id=
+
+dfwm = pd.read_csv(r"portalWebMaps_Test.csv")
+dfsubset = dfwm[['map_title', 'item_id', 'service_title', 'layer_url', 'share_settings', 'number_of_views']]
 dfsubset_san = dfsubset.sort_values(by=['number_of_views'], ascending=False)
 dfsubset_san['layer_url'] = dfsubset_san['layer_url'].str.replace(r'^.*services/([^/]*)/.*$', r'\1', regex=True)
-dfsubset_san['quant_views'] = (((dfsubset_san['number_of_views'] / 250000) * 80) + 30)
+
+int_value = solara.reactive(30)
 
 map_titles = dfsubset_san['map_title'].unique()
 webMaps = []
 map_title_to_color = {}
 mttc = {}
 for title in map_titles:
-    # Generate random RGB values in the range [0, 255]
     red = random.randint(0, 255)
     green = random.randint(0, 255)
     blue = random.randint(0, 255)
-    # Convert RGB values to a valid color format (e.g., HEX or RGBA)
     color = f'rgba({red},{green},{blue},0.3)'
-    k_color = f'rgba({red},{green},{blue},0.8)' # You can use HEX format if needed
-    # Assign the random color to the map_title
+    k_color = f'rgba({red},{green},{blue},0.8)'
     map_title_to_color[title] = color
     mttc[title] = k_color
     webMaps.append(title)
 
-select_wm = solara.reactive(webMaps)
 dfsubset_san['colors'] = [map_title_to_color[title] for title in dfsubset_san['map_title']]
-dfsubset_san['k_colors'] = [mttc[title] for title in dfsubset_san['map_title']]
-dfsubset_san['map_color'] = 'rgba(155,194,156,0.5)'
-dfsubset_san['service_color'] = 'rgba(199,85,46,0.5)'
+#dfsubset_san['k_colors'] = [mttc[title] for title in dfsubset_san['map_title']]
+dfsubset_san['map_color'] = 'rgba(155,194,156,0.8)'
+dfsubset_san['service_color'] = 'rgba(199,85,46,0.8)'
 
-mapdict = dfsubset_san[['map_title', 'map_color']].to_dict('records')
-layerdict = dfsubset_san[['service_title', 'service_color']].to_dict('records')
 dfcolors = pd.DataFrame()
 dfcolors[['item', 'colors']] = dfsubset_san[['map_title', 'map_color']]
 dfcolors = pd.concat([dfcolors, dfsubset_san[['layer_url', 'service_color']].rename(columns={'layer_url': 'item', 'service_color': 'colors'})], ignore_index=True)
 
-tab_index = solara.reactive(0)
+def datavalues(value):
+    dfsubset_san['quant_views'] = (((dfsubset_san['number_of_views'] / 250000) * 80) + value)
+    updateplot(dfsubset_san)
 
-#spring layout
-G = nx.Graph()
+def updateplot(dfsubset_san):
+    map_titles = dfsubset_san['map_title'].unique()
+    webMaps = []
+    map_title_to_color = {}
+    mttc = {}
+    for title in map_titles:
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        color = f'rgba({red},{green},{blue},0.3)'
+        k_color = f'rgba({red},{green},{blue},0.8)'
+        map_title_to_color[title] = color
+        mttc[title] = k_color
+        webMaps.append(title)
 
-G = nx.from_pandas_edgelist(dfsubset_san, 'map_title', 'service_title')
+    dfsubset_san['map_color'] = 'rgba(155,194,156,0.9)'
+    dfsubset_san['service_color'] = 'rgba(199,85,46,0.9)'
 
-# 1. Convert the NetworkX graph into a Plotly graph object
-pos = nx.spring_layout(G)  # Adjust the layout algorithm as needed
+    G = nx.Graph()
+    G = nx.from_pandas_edgelist(dfsubset_san, 'map_title', 'service_title')
+    pos = nx.spring_layout(G)
 
-edge_x = []
-edge_y = []
-for edge in G.edges():
-    x0, y0 = pos[edge[0]]
-    x1, y1 = pos[edge[1]]
-    edge_x += [x0, x1, None]
-    edge_y += [y0, y1, None]
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x += [x0, x1, None]
+        edge_y += [y0, y1, None]
 
-edge_trace = go.Scatter(
-    x=edge_x,
-    y=edge_y,
-    line=dict(width=0.5, color='#000000'),
-    hoverinfo='none',
-    mode='lines',
-)
-#print(edge_trace)
+    edge_trace = go.Scatter(
+        x=edge_x,
+        y=edge_y,
+        line=dict(width=0.5, color='#000000'),
+        hoverinfo='none',
+        mode='lines',
+    )
 
-node_x = []
-node_y = []
-node_text = []
-node_color = []
+    node_x = []
+    node_y = []
+    node_text = []
+    node_color = []
 
-for node in G.nodes():
-    x, y = pos[node]
-    node_x.append(x)
-    node_y.append(y)
-    node_info = str(node) + "<br># of connections: " + str(len(list(G.neighbors(node))))
-    node_text.append(node_info)
-    if node in dfsubset_san['map_title'].values:
-        node_color.append(dfsubset_san.loc[dfsubset_san['map_title'] == node, 'map_color'].iloc[0])
-    else:
-        node_color.append(dfsubset_san.loc[dfsubset_san['service_title'] == node, 'service_color'].iloc[0])
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+        node_info = str(node) + "<br># of connections: " + str(len(list(G.neighbors(node))))
+        node_text.append(node_info)
+        if node in dfsubset_san['map_title'].values:
+            node_color.append(dfsubset_san.loc[dfsubset_san['map_title'] == node, 'map_color'].iloc[0])
+        else:
+            node_color.append(dfsubset_san.loc[dfsubset_san['service_title'] == node, 'service_color'].iloc[0])
 
-node_trace = go.Scatter(
-    x=node_x,
-    y=node_y,
-    text=node_text,
-    hoverinfo='text',
-    mode='markers+text',
-    marker=dict(
-        #size=[10 + 5 * len(list(G.neighbors(node))) for node in G.nodes()],
-        size=dfsubset_san['quant_views'],
-        color=node_color
-    ),
-)
+    node_trace = go.Scatter(
+        x=node_x,
+        y=node_y,
+        text=node_text,
+        hoverinfo='text',
+        mode='markers+text',
+        marker=dict(
+            # size=[10 + 5 * len(list(G.neighbors(node))) for node in G.nodes()],
+            size=dfsubset_san['quant_views'],
+            color=node_color
+        ),
+    )
 
-# 2. Create the figure using Plotly Graph Objects
-fig = go.Figure(data=[edge_trace, node_trace], layout=go.Layout(
-    showlegend=False,
-    hovermode='closest',
-    margin=dict(b=0, l=0, r=0, t=0),
-    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-    height=975
-))
+    fig = go.Figure(data=[edge_trace, node_trace], layout=go.Layout(
+        showlegend=False,
+        hovermode='closest',
+        margin=dict(b=0, l=0, r=0, t=0),
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        height=975
+    ))
+
+    return solara.FigurePlotly(fig)
+
 
 nodes = []
 links = []
-
 def add_node(node_name):
     if node_name not in nodes:
         nodes.append(node_name)
-
-# Helper function to add links
 def add_link(source, target, value, views, color):
     add_node(source)
     add_node(target)
     add_node(views)
     links.append({"source": nodes.index(source), "target": nodes.index(target), "views": nodes.index(views), "color": color, 'value': value})
 
-# Process the enumerated_items and add nodes and links to the Sankey diagram
 for item in zip(dfsubset_san['map_title'], dfsubset_san['layer_url'], dfsubset_san['number_of_views'], dfsubset_san['service_title'], dfsubset_san['colors']):
     source = f"{item[0]}"
     target = f"{item[1]}: {item[3]}"
     value = 1
-    color = f"{item[4]}"  # Or you can use a value based on some criteria if available in your data
+    color = f"{item[4]}"
     views = f"{item[2]}"
-    add_link(source, target, value, views, color)  # First link from source to target
-    add_link(target, views, value, views, color)  # Second link from target to views
+    add_link(source, target, value, views, color)
+    add_link(target, views, value, views, color)
 
-# Create the Sankey diagram trace with colors for the links
 sankey_trace = go.Sankey(
     arrangement="freeform",
     node=dict(
@@ -150,7 +158,6 @@ sankey_trace = go.Sankey(
     )
 )
 
-# Create the figure and plot the Sankey diagram
 sanfig = go.Figure(data=[sankey_trace])
 
 sanfig.update_layout(title_text="Web Maps Connections to Layers in Portal",
@@ -172,11 +179,24 @@ def Page():
 
             with solara.lab.Tab("Spring Layout", icon_name="mdi-chart-line"):
                 with solara.Card(style="height: 1000px;"):
-
-                    solara.FigurePlotly(fig)
+                    solara.SliderInt("Spring Layout - Node Size", value=int_value, min=30, max=70, on_value=datavalues)
+                    solara.Button("Reset", on_click=lambda: int_value.set(42))
+                    solara.Markdown(f"value: {int_value.value}")
+                    datavalues(int_value.value)
 
         with solara.Sidebar():
-            solara.SelectMultiple("Web Maps", select_wm, webMaps)
-            solara.Markdown(f"**Selected**: {select_wm.value}")
+            solara.Markdown("Access Maps in Portal")
+            df = dfsubset[~dfsubset['map_title'].duplicated()]
+            with solara.Column(gap="12px"):
+                for item in df.itertuples():
+                    solara.Button(
+                        f"{item.map_title}", href=f"https://geo.co.crook.or.us/portal/home/item.html?id={item.item_id}",
+                        color="#D9D9D9"
+                    )
+
+                    #on_click=f"(window.open('https://geo.co.crook.or.us/portal/home/item.html?id={item.item_id}'))",
+            # solara.SelectMultiple("Spring Layout - Layer Type", layers, all_layers)
+            # solara.Markdown(f"**Selected**: {layers.value}")
 
     return main
+
